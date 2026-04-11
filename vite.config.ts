89ea75d3caa44c86eac7ce53838445551a3b82b8
Elias-Chairi <eliaschairi@icloud.com>
2026-04-11 16:49:40 +0200

    test netlify

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import tsconfigPaths from "vite-tsconfig-paths"
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/idata2301-group8-frontend/",
+  //  base: "/idata2301-group8-frontend/",
   plugins: [react(), tsconfigPaths()],
 })

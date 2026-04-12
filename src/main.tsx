98c13d154edd45a11952e12f5ad4633fba937430
Marius Dahl <marius.e.dahl@gmail.com>
2026-04-12 10:24:06 +0200

    added style

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import { BrowserRouter } from 'react-router-dom'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-
+import './style.css'
 const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root')!).render(

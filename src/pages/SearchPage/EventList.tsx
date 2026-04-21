94bde03675f6e97a762365c8a0b260d80db56688
Marius Dahl <marius.e.dahl@gmail.com>
2026-04-21 11:25:56 +0200

    fetching done

--- src/pages/SearchPage/EventList.tsx
+++ src/pages/SearchPage/EventList.tsx
@@ -1,36 +1,68 @@
 import EventCard from 'src/pages/EventCard/EventCard';
+import type { Filters as FiltersType } from "@pages/SearchPage/Filters";
+import { useQuery } from '@tanstack/react-query';
 
 type Event = {
-    id: string;
-    title: string;
-    image: string;
-    category: string;
-    location: string;
-    date: string;
-    price: number;
+  id: string;
+  title: string;
+  image: string;
+  category: string;
+  location: string;
+  date: string;
+  price: number;
 };
 
-const EventList = ({ events }: { events: Event[] }) => {
+async function typedFetch<T>(url: string, params?: Object, options?: RequestInit): Promise<T> {
+  if (params) {
+    const searchParams = new URLSearchParams();
 
-    if (events.length === 0) {
-        return <p>No events found.</p>;
-    }
+    Object.entries(params).forEach(([key, value]) => {
+      searchParams.append(key, String(value));
+    });
 
-    return (
-        <section className="events-grid">
-            {events.map(event => (
-                <EventCard
-                    key={event.id}
-                    href={`/events/${event.id}`}
-                    imgSrc={event.image}
-                    title={event.title}
-                    tags={[event.category, event.location]}
-                    date={event.date}
-                    price={event.price}
-                />
-            ))}
-        </section>
-    );
+    url = `${url}?${searchParams.toString()}`;
+  }
+
+  const response = await fetch(url, options);
+  return response.json();
+}
+
+type Params = { query: string, filters: FiltersType }
+
+const EventList = ({ query, filters }: Params) => {
+
+  const { data: events = [], isLoading, error } = useQuery({
+    queryKey: ['events', filters, query],
+    queryFn: () =>
+      typedFetch<Event[]>("/api/events", { ...filters, q: query })
+  });
+
+  if (isLoading) {
+    return <p>Loading ...</p>
+  }
+  // TODO: temporary
+  if (error) {
+    return <p>{error.message}</p>
+  }
+  if (events.length === 0) {
+    return <p>No events found.</p>;
+  }
+
+  return (
+    <section className="events-grid">
+      {events.map((event: Event) => (
+        <EventCard
+          key={event.id}
+          href={`/events/${event.id}`}
+          imgSrc={event.image}
+          title={event.title}
+          tags={[event.category, event.location]}
+          date={event.date}
+          price={event.price}
+        />
+      ))}
+    </section>
+  );
 };
 
 export default EventList;
--- src/pages/SearchPage/SearchBar.tsx
+++ src/pages/SearchPage/SearchBar.tsx
@@ -1,28 +1,28 @@
 import search from '@assets/icons/search.svg'
 
 type Props = {
-    query: string;
-    setQuery: (q: string) => void;
+  query: string;
+  setQuery: (q: string) => void;
 };
 
 const SearchBar = ({ query, setQuery }: Props) => {
-    return (
-        <div className="search-header">
-            <form className="search-bar-container"
-                onSubmit={(e) => e.preventDefault()} >
-                <input
-                    className="search-input"
-                    type="text"
-                    placeholder=""
-                    value={query}
-                    onChange={(e) => setQuery(e.target.value)}
-                />
-                <button className="search-icon-button" type="submit">
-                    <img className="search-icon" src={search} alt="search icon" />
-                </button>
-            </form>
-        </div>
-    );
+  return (
+    <div className="search-header">
+      <form className="search-bar-container"
+        onSubmit={(e) => e.preventDefault()} >
+        <input
+          className="search-input"
+          type="text"
+          placeholder=""
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+        />
+        <button className="search-icon-button" type="submit">
+          <img className="search-icon" src={search} alt="search icon" />
+        </button>
+      </form>
+    </div>
+  );
 };
 
 export default SearchBar;

50ff020b5c087f414084fc18a300766cf02ac1f4
Joakim <joaedv97@gmail.com>
2026-04-23 11:57:27 +0200

    about us page

--- src/App.tsx
+++ src/App.tsx
@@ -6,28 +6,30 @@ import { useLocation } from "react-router-dom";
 import HomePage from '@pages/HomePage/HomePage'
 import EventPageLoader from '@pages/EventPage'
 import ScrollToTop from '@utility/ScrollToTop';
+import AboutUsPage from '@pages/AboutUsPage/AboutUsPage';
 
 
 function App() {
-  const location = useLocation();
+    const location = useLocation();
 
-  const isHeroPage =
-    location.pathname === "/" ||
-    location.pathname.startsWith("/events/");
+    const isHeroPage =
+        location.pathname === "/" ||
+        location.pathname.startsWith("/events/");
 
-  return (
-    <>
-      <TopBar className={isHeroPage ? "topbar-overlay" : ""} />
-      <ScrollToTop />
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/events/:eventslug" element={<EventPageLoader />} />
-        <Route path="/search" element={<SearchPage />} />
-      </Routes>
+    return (
+        <>
+            <TopBar className={isHeroPage ? "topbar-overlay" : ""} />
+            <ScrollToTop />
+            <Routes>
+                <Route path="/" element={<HomePage />} />
+                <Route path="/events/:eventslug" element={<EventPageLoader />} />
+                <Route path="/search" element={<SearchPage />} />
+                <Route path="/aboutUs" element={<AboutUsPage />} />
+            </Routes>
 
-      <Footer />
-    </>
-  );
+            <Footer />
+        </>
+    );
 }
 
 export default App

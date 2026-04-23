9f2b7249b7208298b80162c96d51ea2a862c0125
Joakim <joaedv97@gmail.com>
2026-04-23 11:04:25 +0200

    Now searches on enter input, not every button press

--- src/pages/SearchPage/SearchBar.tsx
+++ src/pages/SearchPage/SearchBar.tsx
@@ -1,28 +1,35 @@
 import search from '@assets/icons/search.svg'
+import { useState } from "react"
+import React from 'react'
 
 type Props = {
-  query: string;
-  setQuery: (q: string) => void;
+    query: string;
+    setQuery: (q: string) => void;
 };
 
 const SearchBar = ({ query, setQuery }: Props) => {
-  return (
-    <div className="search-header">
-      <form className="search-bar-container"
-        onSubmit={(e) => e.preventDefault()} >
-        <input
-          className="search-input"
-          type="text"
-          placeholder=""
-          value={query}
-          onChange={(e) => setQuery(e.target.value)}
-        />
-        <button className="search-icon-button" type="submit">
-          <img className="search-icon" src={search} alt="search icon" />
-        </button>
-      </form>
-    </div>
-  );
+    const [inputValue, setInputValue] = useState(query);
+
+    const handleSubmit = (e: React.FormEvent) => {
+        e.preventDefault();
+        setQuery(inputValue);
+    };
+
+    return (
+        <div className="search-header">
+            <form className="search-bar-container" onSubmit={handleSubmit}>
+                <input
+                    className="search-input"
+                    type="text"
+                    value={inputValue}
+                    onChange={(e) => setInputValue(e.target.value)}
+                />
+                <button className="search-icon-button" type="submit">
+                    <img className="search-icon" src={search} alt="search icon" />
+                </button>
+            </form>
+        </div>
+    );
 };
 
 export default SearchBar;

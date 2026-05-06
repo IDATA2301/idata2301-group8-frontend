f1f031c56b4811a994ce235bd33dcb96e6035a5c
Elias-Chairi <eliaschairi@icloud.com>
2026-05-06 18:26:37 +0200

    updated vite config

--- vite.config.ts
+++ vite.config.ts
@@ -16,21 +16,20 @@ export default defineConfig(({ mode }) => {
         '/api-iam': {
           target: env.IAM_API_URL,
           changeOrigin: true,
-          rewrite: (path) => path.replace(/^\/api-iam/, ''),
-          configure: (proxy, options) => {
-            proxy.on('error', (err, _req, _res) => {
-              console.log('error', err);
-            });
-            proxy.on('proxyReq', (proxyReq, req, _res) => {
-              console.log('Request sent to target:', req.method, req.url);
-            });
-            proxy.on('proxyRes', (proxyRes, req, _res) => {
-              console.log('Response received from target:', proxyRes.statusCode, req.url);
-            });
-          }
+          rewrite: (path) => path.replace(/^\/api-iam/, '')
+        },
+        '/api-event': {
+          target: env.EVENT_API_URL,
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api-event/, '')
+        },
+        '/api-order': {
+          target: env.ORDER_API_URL,
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api-order/, '')
         }
       }
     }
-  }
+  };
 })
 

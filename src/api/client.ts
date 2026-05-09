a393c11914b779b169955505fc3e7fa3588ebc4b
Elias-Chairi <eliaschairi@icloud.com>
2026-05-09 17:33:24 +0200

    updated iam-api and formatting

--- src/api/client.ts
+++ src/api/client.ts
@@ -1,10 +1,14 @@
-export const customFetch = async <T>(baseUrl: string, url: string, options: RequestInit = {}): Promise<T> => {
-  const jwt = localStorage.getItem('jwt');
+export const customFetch = async <T>(
+  baseUrl: string,
+  url: string,
+  options: RequestInit = {},
+): Promise<T> => {
+  const jwt = localStorage.getItem("jwt");
 
   const res = await fetch(baseUrl + url, {
     ...options,
     headers: {
-      'Content-Type': 'application/json',
+      "Content-Type": "application/json",
       ...(options.headers || {}),
       ...(jwt ? { Authorization: `Bearer ${jwt}` } : {}),
     },
@@ -16,31 +20,40 @@ export const customFetch = async <T>(baseUrl: string, url: string, options: Requ
     throw {
       status: res.status,
       data: text || `API error: ${res.status}`,
-      headers: res.headers
+      headers: res.headers,
     } as T;
   }
 
   let json: any;
   try {
-    json = await res.json()
+    json = await res.json();
   } catch (e) {
     json = null;
   }
   return {
     status: res.status,
     data: json,
-    headers: res.headers
+    headers: res.headers,
   } as T;
-}
+};
 
-export const customFetchEventApi = async <T>(url: string, options: RequestInit = {}): Promise<T> => {
-  return customFetch(import.meta.env.VITE_EVENT_API_URL, url, options)
-}
+export const customFetchEventApi = async <T>(
+  url: string,
+  options: RequestInit = {},
+): Promise<T> => {
+  return customFetch(import.meta.env.VITE_EVENT_API_URL, url, options);
+};
 
-export const customFetchIamApi = async <T>(url: string, options: RequestInit = {}): Promise<T> => {
-  return customFetch(import.meta.env.VITE_IAM_API_URL, url, options)
-}
+export const customFetchIamApi = async <T>(
+  url: string,
+  options: RequestInit = {},
+): Promise<T> => {
+  return customFetch(import.meta.env.VITE_IAM_API_URL, url, options);
+};
 
-export const customFetchOrderApi = async <T>(url: string, options: RequestInit = {}): Promise<T> => {
-  return customFetch(import.meta.env.VITE_ORDER_API_URL, url, options)
-}
+export const customFetchOrderApi = async <T>(
+  url: string,
+  options: RequestInit = {},
+): Promise<T> => {
+  return customFetch(import.meta.env.VITE_ORDER_API_URL, url, options);
+};

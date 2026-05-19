fbbde5a36199359dfcfc248aaa2dae95c8592353
Elias-Chairi <eliaschairi@icloud.com>
2026-05-19 10:27:24 +0200

    using hashrouter to support deeplinks in pages

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import './style.css'
 import { AuthProvider } from '@utility/AuthContext'
@@ -11,13 +11,13 @@ const queryClient = new QueryClient()
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
-      <BrowserRouter basename={import.meta.env.BASE_URL}>
+      <HashRouter basename={import.meta.env.BASE_URL}>
         <AuthProvider>
           <ConfirmProvider>
             <App />
           </ConfirmProvider>
         </AuthProvider>
-      </BrowserRouter>
+      </HashRouter>
     </QueryClientProvider>
   </StrictMode>
 )

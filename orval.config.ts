c1c0c8997cc59cdf1208fb987e9c40b221f72be3
Elias-Chairi <eliaschairi@icloud.com>
2026-05-19 13:36:19 +0200

    fix refresh

--- orval.config.ts
+++ orval.config.ts
@@ -1,18 +1,18 @@
 export default {
-  // iam: {
-  //   input: 'http://10.212.26.218/iam/v3/api-docs',
-  //   output: {
-  //     tsconfig: './tsconfig.app.json',
-  //     target: './src/api/iam.ts',
-  //     client: 'react-query',
-  //     override: {
-  //       mutator: {
-  //         path: './src/api/client.ts',
-  //         name: 'customFetchIamApi'
-  //       }
-  //     }
-  //   }
-  // },
+  iam: {
+    input: 'http://64.226.83.250/iam/v3/api-docs',
+    output: {
+      tsconfig: './tsconfig.app.json',
+      target: './src/api/iam.ts',
+      client: 'react-query',
+      override: {
+        mutator: {
+          path: './src/api/client.ts',
+          name: 'customFetchIamApi'
+        }
+      }
+    }
+  },
 
   events: {
     input: "http://localhost:8080/v3/api-docs",

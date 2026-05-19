fbd3b790a309f934f64a913a9f7c945f8bf3766a
Elias-Chairi <eliaschairi@icloud.com>
2026-05-19 10:37:56 +0200

    removed base name

--- src/main.tsx
+++ src/main.tsx
@@ -11,7 +11,7 @@ const queryClient = new QueryClient()
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
-      <HashRouter basename={import.meta.env.BASE_URL}>
+      <HashRouter>
         <AuthProvider>
           <ConfirmProvider>
             <App />
